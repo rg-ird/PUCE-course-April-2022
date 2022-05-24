ea63d04c37f87bc35b73c1e4535453b32bfe510f
romain Guyot <65174045+rg-ird@users.noreply.github.com>
2022-05-24 10:25:12 +0200

    Update CP_assembly.sh

--- CP_assembly.sh
+++ CP_assembly.sh
@@ -8,16 +8,16 @@ echo "bowtie2-build"
 bowtie2-build -f NC_024511.2.fasta NC_024511.2.fasta;
 
 echo "bowtie2"
-bowtie2 -p 2 --no-unal --very-fast-local --al-conc ./${1}_Cp -q -x ./NC_024511.2.fasta -1 ${1}_R1.fq.gz -2 ${1}_R2.fq.gz > ${1}_Cp.sam
+bowtie2 -p 2 --no-unal --very-fast-local --al-conc ./${1}_Cp -q -x ./NC_024511.2.fasta -1 ${1}_R1.fq.gz -2 ${1}_R2.fq.gz > ${1}_Cp.sam;
 
 #filtering - eliminating redundant reads. 
 #This step is optional but it can sped up execution. bbmap scripts must be within its own folder in the precedent folder or the path changed correspondently. They can be downloaded from https://sourceforge.net/projects/bbmap/files/latest/download and installed locally.
 
 echo "BBMAP"
-clumpify.sh in1=./${1}_Cp.1 in2=./${1}_Cp.2 out1=./${1}_Cp_dedup.1 out2=./${1}_Cp_dedup.2 dedupe=t
+clumpify.sh in1=./${1}_Cp.1 in2=./${1}_Cp.2 out1=./${1}_Cp_dedup.1 out2=./${1}_Cp_dedup.2 dedupe=t;
 
 #assembly using NovoPlasty:
 #The configuration file must be renamed to match the $1 variable, and edited to contain the correct file paths and names. Program must be placed in the precedent folder or the path changed correspondently
 echo "Novoplasty"
 
-perl ./NOVOPlasty4.3.1.pl -c config_plasty_${1}.txt
+perl ./NOVOPlasty4.3.1.pl -c config_plasty_${1}.txt;
